aeeb1b3176918c3313879c9711b64258fd3bfb40
Jakub Chodubski <ch.jakub23@gmail.com>
2024-05-27 02:56:54 +0200

    converted to PWA app
    added meta tags and converted app to PWA with specific CSS styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import WebsiteUpdateImage from "../../public/assets/imgs/general/website-update.svg";
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<img style={{width: "28%"}} src={WebsiteUpdateImage.src} alt="website-update" />
-			<div></div>
+			<div>Hello, world!</div>
 		</main>
 	);
 }
